fec5f9491ed2a292fa30004c6c8c800234f81933
Meliwethu Sibanda <melisibs1@gmail.com>
2023-02-18 23:17:13 -0800

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+
 /**
  * main - Entry point
  * 
  * Return: Always 0 (Sucess)
  */
+
 int main(void)
 {
 	char low;
 
-	for(low = 'a'; low <= 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-		putchar("\n");
+		putchar('\n');
 
 	return(0);
 }
